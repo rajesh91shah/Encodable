676eca5b4a200aae308b0d464ef15e3b8bf0dfe6
Rajesh Shah <apple@Tests-MacBook-Pro.local>
2019-11-14 21:52:52 +0530

    first push

---
+++ Encodable/Encodable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Encodable
+//
+//  Created by Rajesh Shah on 14/11/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
